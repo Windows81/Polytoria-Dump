68c24d907957c562b56f2c3b47d6e01e50063c6b
Windows81 <nastyahmede@gmail.com>
2026-02-16 08:15:21 +0000

    2026-02-16T0815Z

--- _.sh
+++ _.sh
@@ -1,9 +1,12 @@
 curl -A "PolytoriaLauncher/4.13" "https://api.polytoria.com/v1/launcher/updates?os=windows&release=stable" | jq -M >windows-updates.json
 
+rm -rf ./Downloads/
 jq -r 'to_entries.[] | (.key? + "|" + .value.Download?)' windows-updates.json | awk -F'|' '{ system("wget --directory-prefix Downloads/\"" $1 "\" -U PolytoriaLauncher/4.13 --no-clobber \"" $2 "\"") }'
 
 ls Downloads | xargs -P3 -I{} 7z x -y "Downloads/{}/*" -o"Unzipped/{}/*"
 
 wget https://github.com/Windows81/AssetRipper-CLI/releases/download/v20260209T200800Z/AssetRipper.CLI.Free.exe
+rm -rf './Unpacked/Client'
 ./AssetRipper.CLI.Free.exe -InputPath './Unzipped/Client' -OutputPath './Unpacked/Client'
+rm -rf './Unpacked/Creator'
 ./AssetRipper.CLI.Free.exe -InputPath './Unzipped/Creator' -OutputPath './Unpacked/Creator'
