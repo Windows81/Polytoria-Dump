aa554f2cc44441b06e121cf5e744e9426fae8e02
Windows81 <nastyahmede@gmail.com>
2026-02-24 05:50:18 +0000

    2026-02-24T0550Z

--- _.sh
+++ _.sh
@@ -9,6 +9,13 @@ awk -F'|' '{ system("wget --directory-prefix Downloads/\"" $1 "\" -U PolytoriaLa
 
 find './Downloads/*' -maxdepth 0 -printf "%f\n" | xargs -P3 -I{} 7z x -y "Downloads/{}/*" -o"Unzipped/{}/*"
 
+
+# Extract downloader script.
+7z e -i!'resources/app.asar' './Unzipped/Installer/*/$PLUGINSDIR/app-64.7z' -aoa -o'./Unzipped/Installer'
+npm install -g asar webcrack
+asar e './Unzipped/Installer/app.asar' './Unpacked/Installer'
+webcrack './Unpacked/Installer/src/main/index.js' >'./Unpacked/Installer/src/main/index-deobfuscated.js'
+
 # Use AssetRipper to extract source code.
 wget https://github.com/Windows81/AssetRipper-CLI/releases/download/v20260222T003813Z/AssetRipper.CLI.Free.exe --no-clobbers
 rip_assets() {
